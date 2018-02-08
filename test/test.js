4a3efa94752cb4cc96b799b10edfe6ee25297faf
rjmcbain <rmcbain01@gmail.com>
2018-02-08 10:02:26 -0700

    adding test

---
+++ test/test.js
@@ -0,0 +1,11 @@
+
+const expect = require('chai').expect;
+const request = require('request');
+
+
+
+describe('Server', function () {
+    it('should pass when everything is okay', function () {
+        expect(true).to.be.true;
+    });
+});
